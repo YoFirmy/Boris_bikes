119635cd88783fc0a1998e95d6ada6074f0edfc0
Matt Firmston <mattfirmston@gmail.com>
2021-07-01 18:33:25 +0100

    written test to check an error is raised if release_bike is called on a DockingStation with no bikes. and made it pass

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,14 @@
 require_relative 'bike'
 
 class DockingStation
-    def release_bike
-        Bike.new
-    end
+  def release_bike
+    fail 'No bikes available' unless @bike
+    @bike
+  end
 
-    def dock(bike)
-        @bike = bike
-    end
+  def dock(bike)
+    @bike = bike
+  end
 
-    attr_reader :bike
+  attr_reader :bike
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,25 +1,36 @@
 require 'docking_station'
 
 describe DockingStation do 
-    it { is_expected.to respond_to(:release_bike) }
-
-    it "releases working bikes" do
-        bike = Bike.new
-        expect(bike).to be_working
+  describe '#release_bike' do
+    it "releases a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq(bike)
+    end 
+    
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
     end
+  end
 
-    it { is_expected.to respond_to(:dock).with(1).argument }
+  it "releases working bikes" do
+    bike = Bike.new
+    expect(bike).to be_working
+  end
 
-    it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:dock).with(1).argument }
 
-    it "docks a bike" do
-        bike = Bike.new
-        expect(subject.dock(bike)).to eq(bike)
-    end
+  it { is_expected.to respond_to(:bike) }
 
-    it "returns docked bikes" do
-        bike = Bike.new
-        subject.dock(bike)
-        expect(subject.bike).to eq(bike)
-    end
+  it "docks a bike" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq(bike)
+  end
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq(bike)
+  end
+
 end
