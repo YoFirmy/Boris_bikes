5f4fe0dc28ca1b3de3d95eb895a6d028b9c06815
Matt Firmston <mattfirmston@gmail.com>
2021-07-01 23:05:39 +0100

    changed capacity of docking stations to 20 using TDD process

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,20 @@
 require_relative 'bike'
 
 class DockingStation
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bikes.length < 1
+    @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station is full' if @bike
-    @bike = bike
+    fail 'Docking station is full' unless @bikes.length < 20
+    @bikes << bike
+    bike
   end
 
-  attr_reader :bike
+  attr_reader :bikes
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,8 @@ require 'docking_station'
 describe DockingStation do 
   describe '#release_bike' do
     it "releases a bike" do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq(bike)
+      subject.dock(Bike.new)
+      expect(subject.release_bike).to be_instance_of(Bike)
     end 
     
     it 'raises an error when there are no bikes available' do
@@ -22,23 +21,22 @@ describe DockingStation do
     it { is_expected.to respond_to(:dock).with(1).argument }
 
     it "docks a bike" do
-      bike = Bike.new
-      expect(subject.dock(bike)).to eq(bike)
+      expect(subject.dock(Bike.new)).to be_instance_of(Bike)
     end
 
     it "raises an error when docking station is at capacity" do
-      subject.dock(Bike.new)
+      20.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error 'Docking station is full'
     end
   end
 
-  describe "#bike" do 
-    it { is_expected.to respond_to(:bike) } 
+  describe "#bikes" do 
+    it { is_expected.to respond_to(:bikes) } 
 
     it "returns docked bikes" do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.bike).to eq(bike)
+      expect(subject.bikes).to eq([bike])
     end
   end
 end
